ad960e9b1a0c5b16441e90798b993cb090316dac
Rainer Poisel <rainer.poisel@gmail.com>
2022-01-05 22:44:48 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#[tokio::main(flavor = "current_thread")]
+pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    use tokio_serial::SerialStream;
+
+    use tokio_modbus::prelude::*;
+
+    let tty_path = "/dev/ttyUSB0";
+    let slave = Slave(0x17);
+
+    let builder = tokio_serial::new(tty_path, 19200);
+    let port = SerialStream::open(&builder).unwrap();
+
+    let mut ctx = rtu::connect_slave(port, slave).await?;
+    println!("Reading a sensor value");
+    let rsp = ctx.read_holding_registers(0x082B, 2).await?;
+    println!("Sensor value is: {:?}", rsp);
+
+    Ok(())
+}
