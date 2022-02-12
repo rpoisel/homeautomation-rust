7e141ad936033c369882cec28e245f9225397877
Rainer Poisel <rainer.poisel@gmail.com>
2022-02-13 00:13:13 +0100

    chore(Rust): use Vec<bool> instead of u8

--- src/main.rs
+++ src/main.rs
@@ -16,22 +16,18 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut ctx_di = rtu::connect_slave(port_di, slave_di).await?;
     let mut interval = time::interval(Duration::from_millis(50));
 
-    let mut states_do: u8 = 0;
+    let mut states_do = Vec::from([false, false, false, false, false, false, false, false]);
     let mut states_prev_di = ctx_di.read_discrete_inputs(0x0000, 8).await?;
     loop {
         let states_di = ctx_di.read_discrete_inputs(0x0000, 8).await?;
 
         for cur in 0..=7 {
-            let mask: u8 = 0x01 << cur;
-
             if !(states_di[cur] && !states_prev_di[cur]) {
                 continue;
             }
 
-            states_do ^= mask;
-            let val_do_byte = states_do & mask;
-            let val_do_bit = val_do_byte == mask;
-            ctx_do.write_single_coil(cur as u16, val_do_bit).await?;
+            states_do[cur] = !states_do[cur];
+            ctx_do.write_single_coil(cur as u16, states_do[cur]).await?;
         }
         states_prev_di = states_di;
 
