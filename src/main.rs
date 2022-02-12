e04492be8e240aaa70b0897caf01f2901b4fc95d
Rainer Poisel <rainer.poisel@gmail.com>
2022-02-13 00:03:07 +0100

    chore(Rust): update Rust code to work with Wellpro WP8026ADA modules

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let tty_path = "/dev/ttyUSB0";
     let slave_do = Slave(0x01);
-    let slave_di = Slave(0x02);
+    let slave_di = Slave(0x01);
 
     let builder = tokio_serial::new(tty_path, 9600);
     let port_do = SerialStream::open(&builder).unwrap();
@@ -17,28 +17,23 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut interval = time::interval(Duration::from_millis(50));
 
     let mut states_do: u8 = 0;
-    let mut states_prev_di = ctx_di.read_holding_registers(0x0080, 2).await?[0] as u8;
+    let mut states_prev_di = ctx_di.read_discrete_inputs(0x0000, 8).await?;
     loop {
-        let rsp = ctx_di.read_holding_registers(0x0080, 2).await?;
-        let di_0_states: u8 = rsp[0] as u8;
+        let states_di = ctx_di.read_discrete_inputs(0x0000, 8).await?;
 
-        for cur in 0..8 {
+        for cur in 0..=7 {
             let mask: u8 = 0x01 << cur;
 
-            let val_cur_byte = di_0_states & mask;
-            let val_cur = val_cur_byte == mask;
-            let val_prev_byte = states_prev_di & mask;
-            if !(val_cur && val_prev_byte == 0) {
+            if !(states_di[cur] && !states_prev_di[cur]) {
                 continue;
             }
 
             states_do ^= mask;
-
             let val_do_byte = states_do & mask;
             let val_do_bit = val_do_byte == mask;
-            ctx_do.write_single_coil(cur, val_do_bit).await?;
+            ctx_do.write_single_coil(cur as u16, val_do_bit).await?;
         }
-        states_prev_di = di_0_states;
+        states_prev_di = states_di;
 
         interval.tick().await;
     }
